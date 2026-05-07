bd79f61ed20b3115248bdc8c9162d478d01084e2
Egor Korolev <616empty666@gmail.com>
2026-05-07 16:59:32 +0500

    Добавьте файлы проекта.

---
+++ ExifInput/Class1.cs
@@ -0,0 +1,29 @@
+﻿using ImageMagick;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace ExifInput
+{
+    public class ExifInput
+    {
+
+
+        static void Repalce (ExifTag<string> tag, string path)
+        {
+            MagickImage image = new MagickImage(path);
+            using (image)
+            {
+                var profile = image.GetExifProfile() ?? new ExifProfile();
+
+                // Устанавливаем текстовые значения
+                Console.Write(tag.ToString() + ": ");
+                var str = Console.ReadLine().ToString();
+                profile.SetValue(tag, str);
+                // Добавляем профиль к изображению
+                image.SetProfile(profile);
+                // Сохраняем результат
+                image.Write(path);
+            }
+
+        }
+    }
+}
---
+++ ExifTest/Program.cs
@@ -0,0 +1,26 @@
+﻿using ImageMagick;
+using static System.Net.Mime.MediaTypeNames;
+
+
+
+using (MagickImage image = new MagickImage("C:\\Users\\Empty\\source\\repos\\ExifTest\\ExifTest\\1.png"))
+{
+    var profile = image.GetExifProfile() ?? new ExifProfile();
+
+    // Устанавливаем текстовые значения
+    profile.SetValue(ExifTag.Copyright, Console.ReadLine().ToString());
+    profile.SetValue(ExifTag.Artist, Console.ReadLine().ToString());
+    profile.SetValue(ExifTag.ImageDescription, Console.ReadLine().ToString());
+    // Добавляем профиль к изображению
+    image.SetProfile(profile);
+    // Сохраняем результат
+    image.Write("C:\\Users\\Empty\\source\\repos\\ExifTest\\ExifTest\\out.png");
+}
+
+MagickImage image1 = new MagickImage("C:\\Users\\Empty\\source\\repos\\ExifTest\\ExifTest\\out.png");
+var govno = image1.GetExifProfile();
+
+foreach (var a in govno.Values)
+{
+    Console.WriteLine(a.ToString());
+}

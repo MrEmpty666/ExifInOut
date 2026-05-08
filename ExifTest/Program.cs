205124feae5a6898dbfd363ebb8c96a2b7818bed
Utapok <Utapok@DESKTOP-56P9R9J>
2026-05-08 15:00:43 +0500

    начал создавть оконное приложение

--- ExifTest/Program.cs
+++ ExifTest/Program.cs
@@ -1,17 +1,17 @@
-﻿using ImageMagick;
-using static System.Net.Mime.MediaTypeNames;
-using static ExifInput;
+﻿//using ImageMagick;
+//using static System.Net.Mime.MediaTypeNames;
+//using static ExifInput;
 
-internal class Program
-{
-    private static void Main(string[] args)
-    {
-        var path = Console.ReadLine();
-        WriteExif(path);
-        Console.ReadLine();
-        Repalce(ExifTag.ImageDescription, path);
-        Console.ReadLine();
-        WriteExif(path);
-        Console.ReadLine();
-    }
-}
+//internal class Program
+//{
+//    private static void Main(string[] args)
+//    {
+//        var path = Console.ReadLine();
+//        WriteExif(path);
+//        Console.ReadLine();
+//        Repalce(ExifTag.ImageDescription, path);
+//        Console.ReadLine();
+//        WriteExif(path);
+//        Console.ReadLine();
+//    }
+//}
